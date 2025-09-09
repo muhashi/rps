d39d2871feb1311a9502e2337371c5bea45b85c5
muhashi <105213357+muhashi@users.noreply.github.com>
2025-09-09 11:08:05 +1000

    Add base to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/rps/', // https://vitejs.dev/guide/static-deploy.html#github-pages
   plugins: [react()],
-})
+});
